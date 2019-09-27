bd08824ddd5201bb068a092347a54b93283b4446
Beau Harrison <beau@fnal.gov>
2019-09-27 10:26:51 -0500

    Include ReactiveInput directly in index. This is instead of using App as an intermediary.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactiveInput from './ReactiveInput';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <ReactiveInput label="Hello ICALEPCS 2019" maxLength={10} />,
+    document.getElementById('root')
+);
